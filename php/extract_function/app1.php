c82c0a2508662784e6afaf758a17153217b06d1f
Nikko Enggaliano <37658579+nikkoenggaliano@users.noreply.github.com>
2021-10-06 07:25:25 +0700

    create app1

---
+++ php/extract_function/app1.php
@@ -0,0 +1,22 @@
+<?php 
+
+/*
+
+    Deploy: php -S 0.0.0.0:8000 app1.php
+    Deplpyed: localhost:8000 / ip:8000 
+    Exploit: http://localhost:8000/?status=1
+
+*/
+
+
+$status = false; 
+
+
+extract($_GET);
+
+
+if($status == false){
+    echo "Status unchanged";
+}else{
+    echo "Status changed ", $status;
+}
